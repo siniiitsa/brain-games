35b37dc90747ac63bcc5ac3aeb872f06008598d3
Sergey <siniiitsa@gmail.com>
2020-03-02 10:30:58 +0200

    fix minor issues in brain-even game

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,15 +1,18 @@
 import createGame from '../index.js';
+import { getRandomInt } from '../helpers.js';
 
 const rulesMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
-const getRandomInteger = (limit = 100) => Math.floor(Math.random() * limit) + 1;
+
+const getQuestion = () => getRandomInt(0, 100);
+
 const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
 
 const playBrainEven = createGame({
   rulesMessage,
   getCorrectAnswer,
-  getQuestion: getRandomInteger,
+  getQuestion,
 });
 
 export default playBrainEven;
