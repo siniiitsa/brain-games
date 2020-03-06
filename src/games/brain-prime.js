a0f0394048c76d122c74c0bc693f9a42ffc00ab1
Sergey <siniiitsa@gmail.com>
2020-03-06 21:38:49 +0200

    refactor brain-prime game module

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,34 +4,29 @@ import { getRandomInt } from '../helpers.js';
 const rulesMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 1) {
-    return false;
-  }
+  if (num < 1) return false;
 
   for (let i = 2; i < num / 2; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
+    if (num % i === 0) return false;
   }
 
   return true;
 };
 
-const getQuestion = () => {
+const getQuestionData = () => {
   const min = 1;
   const max = 100;
-  return getRandomInt(min, max);
-};
 
-const getCorrectAnswer = (question) => {
-  const num = Number(question);
-  return isPrime(num) ? 'yes' : 'no';
+  const num = getRandomInt(min, max);
+  return {
+    question: String(num),
+    answer: isPrime(num) ? 'yes' : 'no',
+  };
 };
 
 const playBrainPrime = createGame({
   rulesMessage,
-  getCorrectAnswer,
-  getQuestion,
+  getQuestionData,
 });
 
 export default playBrainPrime;
