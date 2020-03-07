c4d82463f9159c108aa60d6ef2aff2aeba53f0bb
Sergey <siniiitsa@gmail.com>
2020-03-07 22:58:23 +0200

    refactor brain-even game module

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,10 +3,13 @@ import { getRandomInt } from '../helpers.js';
 
 const rulesMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const minNum = 1;
+const maxNum = 100;
+
 const isEven = (number) => number % 2 === 0;
 
 const getQuestionData = () => {
-  const number = getRandomInt(0, 100);
+  const number = getRandomInt(minNum, maxNum);
   return {
     question: String(number),
     answer: isEven(number) ? 'yes' : 'no',
