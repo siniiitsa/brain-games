eb86cef3ca4bf949039bf614837a1a88bc7a8d8e
Sergey <siniiitsa@gmail.com>
2020-03-12 11:39:11 +0200

    remove unnecessary stringToNum function from helpers file

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 export const getRandomInt = (min, max) => (
   Math.floor(min + Math.random() * (max + 1 - min))
 );
-
-export const stringToNum = (string) => Number(string) || string;
