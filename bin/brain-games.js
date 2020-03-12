98a7a949594646e2e7010cc0cd8bf5dfe1e6ccc6
Sergey <siniiitsa@gmail.com>
2020-03-12 23:16:57 +0200

    delete bin/brain-games.js file

--- bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}`);
